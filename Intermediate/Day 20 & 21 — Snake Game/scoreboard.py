bcae7adfc88cd31cf9b550fc1e930435cf4efb78
may-95 <ions-super.0p@icloud.com>
2023-03-22 13:39:34 +0000

    added high score tracking
    updated the game and added high score tracking feature using day 24 knowledge.

--- Intermediate/Day 20 & 21 — Snake Game/scoreboard.py
+++ Intermediate/Day 20 & 21 — Snake Game/scoreboard.py
@@ -9,6 +9,8 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
         self.color("white")
         self.hideturtle()
         self.penup()
@@ -17,10 +19,14 @@ class Scoreboard(Turtle):
 
     # writes score onto screen.
     def write_score(self):
-        self.write(f"Score: {self.score}", align=ALIGN, font=FONT)
+        self.write(f"Score: {self.score}   High Score: {self.high_score}", align=ALIGN, font=FONT)
 
     # prints GAME OVER message.
     def game_over(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(f"{self.high_score}")
         self.goto(0, 0)
         self.write("GAME OVER!", align=ALIGN, font=FONT)
 
