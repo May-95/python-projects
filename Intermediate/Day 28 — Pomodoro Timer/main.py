0c93828b49cbda04d45c6cd97ddc9f202c1a739c
may-95 <ions-super.0p@icloud.com>
2023-03-23 18:00:16 +0000

    Pomodoro Timer

---
+++ Intermediate/Day 28 — Pomodoro Timer/main.py
@@ -0,0 +1,91 @@
+# Day 28 -- Pomodoro Timer
+
+from tkinter import *
+import math
+
+# ---------------------------- CONSTANTS ------------------------------- #
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 0.1
+SHORT_BREAK_MIN = 0.1
+LONG_BREAK_MIN = 0.1
+timer = None
+reps = 0
+check_mark = "✔"
+
+
+# ---------------------------- TIMER RESET ------------------------------- #
+# resets all the labels to their original values and stops the timer.
+def reset_timer():
+    global reps
+    reps = 0
+    window.after_cancel(timer)
+    timer_label.config(text="Timer", fg=GREEN)
+    done_checks.config(text="")
+    canvas.itemconfig(timer_text, text="00:00")
+
+
+# ---------------------------- TIMER MECHANISM ------------------------------- #
+# triggers the correct timer depending on the current rep the user is on.
+def start_timer():
+    global reps
+    reps += 1
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+
+    if reps % 8 == 0:
+        timer_label.config(text="Break", fg=RED)
+        countdown(long_break_sec)
+    elif reps % 2 == 0:
+        timer_label.config(text="Break", fg=PINK)
+        countdown(short_break_sec)
+    else:
+        timer_label.config(text="Timer", fg=GREEN)
+        countdown(work_sec)
+
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+# countdown to 0 and adds a checkmark for the amount of reps completed.
+def countdown(count):
+    global timer
+    count_min = math.floor(count / 60)
+    count_sec = round(count % 60)
+    if count_sec < 10:
+        count_sec = f"0{count_sec}"
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
+    if count > -1:
+        timer = window.after(1000, countdown, count - 1)
+    else:
+        reps_completed = math.floor(reps / 2)
+        done_checks.config(text=f"{check_mark * reps_completed}")
+        start_timer()
+
+
+# ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Pomodoro Timer")
+window.config(pady=100, padx=50, bg=YELLOW)
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+img = PhotoImage(file="tomato.png")
+canvas.create_image(100, 112, image=img)
+timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.grid(column=1, row=1)
+
+timer_label = Label(text="Timer", font=(FONT_NAME, 50), fg=GREEN, bg=YELLOW)
+timer_label.config(pady=20)
+timer_label.grid(column=1, row=0)
+
+start_button = Button(text="Start", highlightthickness=0, bg=YELLOW, command=start_timer)
+start_button.grid(column=0, row=2)
+
+done_checks = Label(text="", font=(FONT_NAME, 20, "bold"), bg=YELLOW, fg=GREEN)
+done_checks.grid(column=1, row=3)
+
+reset_button = Button(text="Reset", highlightthickness=0, command=reset_timer)
+reset_button.grid(column=3, row=2)
+
+window.mainloop()
